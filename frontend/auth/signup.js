8aea49cefe420fbaa3aef1bcac6a9ab1d8443ce6
Suyesh786 <suyeshkaranam786@gmail.com>
2026-04-15 20:31:38 +0530

    fixed signup

--- frontend/auth/signup.js
+++ frontend/auth/signup.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
 
-      const response = await fetch("http://idsnext-backend.onrender.com/api/auth/register", {
+      const response = await fetch("https://idsnext-backend.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password, section })
